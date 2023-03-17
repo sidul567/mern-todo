01107aae8858cba79035ffaaa824065ea909bf53
sidul567 <sidulsam@gmail.com>
2023-03-17 16:45:04 +0600

    Add toaster and loader

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import {UilTimes} from '@iconscout/react-unicons';
 import { useEffect, useState } from 'react';
+import {ThreeCircles} from 'react-loader-spinner';
+import {ToastContainer, toast} from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
 
   const [todos, setTodos] = useState([]);
   const [popup, setPopup] = useState(false);
   const [newTodo, setNewTodo] = useState("");
+  const [loading, setLoading] = useState(false);
 
   useEffect(()=>{
     getTodos();
@@ -13,9 +17,10 @@ function App() {
 
   async function getTodos(){
     try{
+      setLoading(true);
       const response = await fetch('https://mern-todo-vert.vercel.app/');
       const data = await response.json();
-      console.log(data);
+      setLoading(false);
       setTodos(data);
     }catch(err){
       console.log(err);
@@ -25,6 +30,7 @@ function App() {
   async function createTodo(){
     try{
       if(newTodo.trim() !== ""){
+        setLoading(true);
         const response = await fetch('https://mern-todo-vert.vercel.app/',{
         'method': 'POST',
         'headers': {
@@ -35,6 +41,17 @@ function App() {
         })
       })
         const data = await response.json();
+        setLoading(false);
+        toast.success('Todo Added Successfully!', {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
         setTodos([...todos, data]);
         setPopup(false);
         setNewTodo("");
@@ -46,12 +63,23 @@ function App() {
   
   async function deleteTodo(todoId){
     try{
+      setLoading(true);
       const response = await fetch(`https://mern-todo-vert.vercel.app/${todoId}`,{
         'method': 'DELETE',
       });
 
       const data = await response.json();
-
+      setLoading(false);
+      toast.success('Todo Delete Successfully!', {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
       setTodos(todos=>todos.filter(todo=>todo._id !== data._id));
     }catch(err){
       console.log(err);
@@ -60,12 +88,13 @@ function App() {
 
     async function completeTodo(todoId){
     try{
+      setLoading(true);
       const response = await fetch(`https://mern-todo-vert.vercel.app/${todoId}`,{
       'method': 'PUT'
     });
 
       const data = await response.json();
-
+      setLoading(false);
       setTodos(todos=>todos.map(todo=>{
         if(todo._id === data._id){
           todo.complete = data.complete;
@@ -77,18 +106,55 @@ function App() {
     }
   }
 
+  console.log(todos); 
+
   return (
     <div className="container">
+      <ToastContainer 
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
       <h1>My todos</h1>
       <div className="todos">
         {
-          todos && todos.map((todo)=>(
+          loading && (
+            <div className="spinner">
+              <ThreeCircles
+                height="50"
+                width="50"
+                color="#8F43EE"
+                wrapperStyle={{}}
+                wrapperClass=""
+                visible={true}
+                ariaLabel="three-circles-rotating"
+                outerCircleColor=""
+                innerCircleColor=""
+                middleCircleColor=""
+              />
+            </div>
+          )
+        }
+        {
+          todos.length > 0 && !loading && todos.map((todo)=>(
             <div className={todo.complete? "todo is-completed" : "todo"} key={todo._id} onClick={()=>completeTodo(todo._id)}>
               <div className="checkbox"></div>
               <div className="text">{todo.text}</div>
               <div className="delete" onClick={()=>deleteTodo(todo._id)}><UilTimes /></div>
             </div>
           ))
+        }
+        {
+          todos.length === 0 && !loading && (
+            <div className="no-todo">Empty Todos!</div>
+          )
         }
       </div>
 
